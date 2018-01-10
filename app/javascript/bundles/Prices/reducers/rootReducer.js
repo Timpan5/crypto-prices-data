d313df9addc257dfbf1331347031fc8a2e8027d0
Timothy Pan <timothy.pan@sierra-mba.local>
2018-01-09 19:08:33 -0500

    Limit max number of chart data points

--- app/javascript/bundles/Prices/reducers/rootReducer.js
+++ app/javascript/bundles/Prices/reducers/rootReducer.js
@@ -3,6 +3,8 @@ import { fromJS, List, Map } from 'immutable';
 
 import * as priceConstants from '../constants/pricesConstants';
 
+const maxChartDataPoints = 25;
+
 const initialPrices = fromJS({
   'BTC': [],
 });
@@ -11,12 +13,17 @@ const initialStoreState = new Map({
   prices: initialPrices,
 });
 
+function coinPriceUpdate(state, action) {
+  return state.updateIn(['prices', action.coin], (list) =>
+    list.size >= maxChartDataPoints ? list.shift().push(action.price) : list.push(action.price));
+}
+
 const display = (state = initialStoreState, action) => {
   switch (action.type) {
     // case priceConstants.COIN_NAME_UPDATE:
     //   return state.prices.update('name', () => action.text);
     case priceConstants.COIN_PRICE_UPDATE:
-      return state.updateIn(['prices', action.coin], (list) => list.push(action.price));
+      return coinPriceUpdate(state, action);
     default:
       return state;
   }
